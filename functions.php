29647d693656fb1f069a91ad2583cf536257cea8
Khawm Mung <johnmungtawng5@gmail.com>
2022-07-25 19:22:00 -0500

    cdn > localization

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@
         // dynamically add page title
         add_theme_support('title-tag');
     }
-
     add_action('after_setup_theme', 'yd_title');
     
 
@@ -14,7 +13,7 @@
         wp_enqueue_style('custom-style', get_template_directory_uri() . '/style.css', array('zyd-bootstrap'), $version, 'all');
 
         //bootstrap css
-        wp_enqueue_style('zyd-bootstrap',  get_template_directory_uri() . '/css/bootstrap.min.css', array(), '', 'all');
+        wp_enqueue_style('zyd-bootstrap',  'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous', array(), '5.2', 'all');
     }
 
     add_action('wp_enqueue_scripts', 'load_stylesheets');
