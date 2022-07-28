17f16f10115fc013712851bac656685507d2762a
Khawm Mung <johnmungtawng5@gmail.com>
2022-07-28 15:56:10 -0500

    Update search.php

--- search.php
+++ search.php
@@ -1,1 +1,21 @@
-<?php //silence is golden
+<?php get_header(); ?>
+
+    <div>
+        <h2>Search results</h2>
+        <?php
+            if (have_posts()) {
+                while (have_posts()) {
+                    the_posts();
+                    the_title(sprintf(
+                        '<h3><a href="%s">',
+                        get_permalink()
+                    ), '</a></h3>');
+                    the_excerpt();
+                    ?>
+                    <hr>
+                    <?php
+                }
+            }
+        ?>
+    </div>
+<?php get_footer(); ?>
