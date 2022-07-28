ff4ddbef52a3b874e146dd55eb5a51ca03eafa74
Khawm Mung <johnmungtawng5@gmail.com>
2022-07-28 15:59:39 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,1 +1,10 @@
-<?php // silence is golden
+<?php get_header(); ?>
+<article class="content px-3 py-5 p-md-5">
+    <h1>Page Not Found</h1>
+
+    <!--Search form-->
+    <br>
+    <h2>Perhaps try searching</h2>
+    <?php get_search_form(); ?>
+</article>
+<?php get_footer(); ?>
