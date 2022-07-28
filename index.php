562aa67fba606b6fe7e59a6a48bccd017f3b5c6d
Khawm Mung <johnmungtawng5@gmail.com>
2022-07-28 16:27:35 -0500

    archive page

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php //silence is golden
+<?php get_header();?>
+
+<!--Page Title-->   
+<h1><?php the_title();?></h1>
+
+<article class="content">
+    <!--Content Section-->
+    <?php 
+    // dynamically add the contents of posts
+    if ( have_posts() ) {
+        while( have_posts() ){
+            the_post();
+            get_template_part('template-parts/content','archive');
+        }
+    }  
+?>
+</article>
+
+<?php get_footer();?>
