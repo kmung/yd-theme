14b37d5f4b3fff7602cfee27bfd49dc165e3a0fb
Khawm Mung <johnmungtawng5@gmail.com>
2022-07-29 00:05:36 -0500

    authorship

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,6 +1,14 @@
 <div class="container">
     <div class="meta mb-3">
         <span class="date"><?php the_date();?></span>
+        <br>
+        <span>By</span>
+        <?php 
+            $firstName = get_the_author_meta('first_name');
+            $lastName = get_the_author_meta('last_name');
+            echo $firstName . ' ' .  $lastName;
+        ?>
     </div>
     <?php the_content(); ?>
+    <br>
 </div>
