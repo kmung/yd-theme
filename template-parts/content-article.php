4d1d8b0d81adf3ffd666c30d6376403bfd5de129
Khawm Mung <johnmungtawng5@gmail.com>
2022-07-29 00:36:41 -0500

    categories and bugs

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -2,15 +2,29 @@
     <div class="meta mb-3">
         <!--Print post date-->
         <span class="date"><?php the_date();?></span>
-        <br>
         <!--Print post author-->
-        <span>By
+        <span>&nbsp;By
         <?php 
             $firstName = get_the_author_meta('first_name');
             $lastName = get_the_author_meta('last_name');
             echo $firstName . ' ' .  $lastName;
         ?>
         </span>
+        <br>
+        <!--Tags Area-->
+        <?php 
+        $tags = get_the_tags();
+        foreach($tags as $tags): ?>
+            <a class="badge bg-success" href="#"> <?php echo $tag->name;?>
+            </a>
+        <?php endforeach;?>
+        
+        <!--Category Area-->
+        <?php
+        $categories = get_the_category();
+        foreach($categories as $categories): ?>
+            <?php echo $categories->name; ?>
+        <?php endforeach; ?>
     </div>
     <!--Post Content-->
     <?php the_content(); ?>
