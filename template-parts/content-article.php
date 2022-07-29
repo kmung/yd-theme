73439941993bd0c91dac1929ed8693b036cd57bd
Khawm Mung <johnmungtawng5@gmail.com>
2022-07-29 00:07:54 -0500

    badazzle

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,14 +1,18 @@
 <div class="container">
     <div class="meta mb-3">
+        <!--Print post date-->
         <span class="date"><?php the_date();?></span>
         <br>
-        <span>By</span>
+        <!--Print post author-->
+        <span>By
         <?php 
             $firstName = get_the_author_meta('first_name');
             $lastName = get_the_author_meta('last_name');
             echo $firstName . ' ' .  $lastName;
         ?>
+        </span>
     </div>
+    <!--Post Content-->
     <?php the_content(); ?>
     <br>
 </div>
