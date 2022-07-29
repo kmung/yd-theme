f83f4e78c8ead64fe5c5e4486bc8075e2b77169d
Khawm Mung <johnmungtawng5@gmail.com>
2022-07-29 11:07:55 -0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <article class="content px-3 py-5 p-md-5">
-    <h1>Page Not Found</h1>
+    <h1 id="404-page-not-found">404 Error</h1>
+    <p>The page you are looking for cannot be found.</p>
 
     <!--Search form-->
     <br>
