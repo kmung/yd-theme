2ac6746f6056c11e63443fffd605b43984826477
Khawm Mung <johnmungtawng5@gmail.com>
2022-08-01 20:31:49 -0500

    page and posts stylesheets

--- functions.php
+++ functions.php
@@ -20,6 +20,8 @@
         // version is a variable that holds the version of the stylesheet, updated in style.css
         $version = wp_get_theme()->get('Version');
         wp_enqueue_style('custom-style', get_template_directory_uri() . '/style.css', array('zyd-bootstrap'), $version, 'all');
+        wp_enqueue_style('page-style', get_template_directory_uri() . '/assets/css/page.css', array('zyd-bootstrap'), $version, 'all');
+        wp_enqueue_style('post-style', get_template_directory_uri() . '/assets/css/post.css', array('zyd-bootstrap'), $version, 'all');
 
         //bootstrap css
         wp_enqueue_style('zyd-bootstrap',  'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous', array(), '5.2', 'all');
