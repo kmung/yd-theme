080450b15a4529032c513ef95443ac39e73b0b08
Khawm Mung <johnmungtawng5@gmail.com>
2022-08-03 09:45:42 -0500

    Update template-contactus.php

--- template-contactus.php
+++ template-contactus.php
@@ -6,21 +6,24 @@ Template Name: Contact Us
 
 <?php get_header();?>
 
-<div class="container">
-    <!--Page Title-->
-    <h1><?php the_title();?></h1>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <p>Get in touch</p>
-        </div>
-        <div class="col-lg-6">
-            <!--Content section-->
-            <?php get_template_part('inc/section','content');?>
-        </div>
-    </div>
-
+<div class="main-wrapper">
+    <div class="page-title theme-bg-light text-center gradient py-5">
+        <!--Page Title-->
+        <h1><?php the_title();?></h1>
 </div>
-
+    <article class="about-section py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <p>Get in touch</p>
+                </div>
+                <div class="col-lg-6">
+                    <!--Content section-->
+                    <?php get_template_part('inc/section','content');?>
+                </div>
+            </div>
+        </div>
+    </article>
+</div>
 
 <?php get_footer();?>
