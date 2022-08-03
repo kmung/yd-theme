c5e169c66d48f4de45d14a48ed6e1387954408c5
Khawm Mung <johnmungtawng5@gmail.com>
2022-08-03 09:54:07 -0500

    stylesheets update

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@
         wp_enqueue_style('custom-style', get_template_directory_uri() . '/style.css', array('zyd-bootstrap'), $version, 'all');
         wp_enqueue_style('page-style', get_template_directory_uri() . '/assets/css/main.css', array('zyd-bootstrap'), $version, 'all');
 
-        //bootstrap css
+        //bootstrap 5 css
         wp_enqueue_style('zyd-bootstrap',  'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous', array(), '5.2', 'all');
     }
     add_action('wp_enqueue_scripts', 'load_stylesheets');
